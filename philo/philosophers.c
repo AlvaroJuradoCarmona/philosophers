c52b2e0033e1736f44ee06bdeb1f7a0751dae538
Alvaro Jurado <quickalvaro@gmail.com>
2024-01-30 14:00:53 +0100

    error fix

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -16,19 +16,19 @@ static void	check_philos_states(t_data *data)
 {
 	t_philo	*aux;
 
-	aux = data->philos;
+	aux = data->philo;
 	while (!check_death(aux) && !meals_eaten(data))
 	{
 		if (aux->next)
 			aux = aux->next;
 		else
-			aux = data->philos;
+			aux = data->philo;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_data	data;
+	t_data	*data;
 
 	if (argc != 5 && argc != 6 && !valid_args(argc, argv))
 		return (-1);
@@ -38,9 +38,15 @@ int	main(int argc, char **argv)
 	memset(data, 0, sizeof(t_data));
 	if (!init_data(argc, argv, data))
 		return (free_all(data), -1);
-	
+	check_philos_states(data);
+	while (data->philo)
+	{
+		pthread_join(data->philo->thread, NULL);
+		pthread_mutex_destroy(&data->philo->fork);
+		data->philo = data->philo->next;
+	}
 	if (data->total_meals == 0
-		|| (!data->philo_dead && data->meals_eaten == data->numphilo))
+		|| (!data->philo_dead && data->eaten_cont == data->numphilo))
 		printf("All philos have eaten %d times\n", data->total_meals);
 	return (free_all(data), 0);
 }
